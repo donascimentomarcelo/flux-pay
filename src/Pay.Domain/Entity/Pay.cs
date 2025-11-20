4fc5118eec0b2e1f1ea743ff19cc48edc7207472
Marcelo Sant'Anna <marcelo.laravel@gmail.com>
2025-11-19 23:50:59 -0300

    Add Pay entity and payment method/status enums

--- src/Pay.Domain/Entity/Pay.cs
+++ src/Pay.Domain/Entity/Pay.cs
@@ -2,16 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Pay.Api.Dto.Enums;
+using Pay.Domain.Dto.Enums;
 
-namespace Pay.Api.Dto
+namespace Pay.Domain.Entity
 {
-    public class CreatePayRequest
+    public class Pay
     {
+        public Guid Id { get; set; } = Guid.NewGuid();
         public decimal Amount { get; set; }
         public PayMethod Method { get; set; }
+        public PayStatus Status { get; set; } = PayStatus.Pending;
+
         public string CustomerName { get; set; }
         public string CustomerEmail { get; set; }
+
         public string ExternalReference { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
     }
 }
---
+++ src/Pay.Domain/Enums/PayMethod.cs
@@ -0,0 +1,9 @@
+namespace Pay.Domain.Dto.Enums
+{
+    public enum PayMethod
+    {
+        Pix = 1,
+        Card = 2,
+        Boleto = 3,
+    }
+}
